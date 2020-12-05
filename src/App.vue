9b95b7449a59674bcd449b4db263cdfe0721cd22
cyberkillua <moshood988@gmail.com>
2020-12-05 21:35:59 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="question-div">
+      <h2 v-if="displayScore">Your final is {{ score }} points</h2>
+      <div v-bind:key="ques.id" v-for="ques in questions">
+        <Displayq :ques="ques" @answer="calScore" />
+      </div>
+      <button @click="showScore">Sumbit</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import Displayq from "./components/Displayq.vue";
+import he from "he";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      questions: [],
+      score: 0,
+      displayScore: false,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Displayq,
+  },
+  methods: {
+    calScore(mark) {
+      this.score = this.score + mark;
+      // {this.answersPicked = this.answersPicked.push(data)}
+      // console.log(this.answersPicked);
+    },
+    showScore() {
+      this.displayScore = true;
+    },
+  },
+
+  created() {
+    axios
+      .get(
+        "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple"
+      )
+      .then((res) => {
+        this.questions = res.data.results.map((questionObject) => ({
+          ...questionObject,
+          question: he.decode(questionObject.question),
+          correct_answer: he.decode(questionObject.correct_answer),
+          incorrect_answers: questionObject.incorrect_answers.map((ans) => {
+            return he.decode(ans);
+          }),
+        }));
+      })
+      .catch((err) => console.log(err));
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+}
+.question-div{
+  /* background-color: yellow; */
+  padding: 25px;
+   line-height: 1.6;
+}
+button {
+  font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #065784;
+  width: 100px;
+  border: 0;
+  justify-self: center;
+  align-self: center;
+  padding: 15px;
+  color: #ffffff;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/Displayq.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div>
+      <p>{{ ques.question }}</p>
+      <!-- <p v-html="ques.question"></p> -->
+      <input
+        type="radio"
+        id="true"
+        :value="ques.incorrect_answers[0]"
+        v-model="picked"
+        @change="emitCustomEvent"
+      />
+      <label for="true">{{ ques.incorrect_answers[0] }}</label>
+      <input
+        type="radio"
+        id="true"
+        :value="ques.incorrect_answers[1]"
+        v-model="picked"
+        @change="emitCustomEvent"
+      />
+      <label for="true">{{ ques.incorrect_answers[1] }}</label>
+      <input
+        type="radio"
+        id="true"
+        :value="ques.incorrect_answers[2]"
+        v-model="picked"
+        @change="emitCustomEvent"
+      />
+      <label for="true">{{ ques.incorrect_answers[2] }}</label>
+      <input
+        type="radio"
+        id="false"
+        :value="ques.correct_answer"
+        v-model="picked"
+        @change="emitCustomEvent"
+      />
+      <label for="false">{{ ques.correct_answer }}</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Displayq",
+  components: {},
+  data() {
+    return {
+      picked: null,
+    };
+  },
+
+  props: ["ques"],
+  methods: {
+    emitCustomEvent() {
+      if (this.picked === this.ques.correct_answer) {
+        console.log("correct");
+        this.$emit("answer", 10);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
